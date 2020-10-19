e8b272021776b0303c9a9694887fda1f5cbfe6e7
hyperinactive <hyperinactive.box@gmail.com>
2020-10-19 22:14:00 +0200

    1.0.4
    Enabled:
    - logging all products
    - delete
    - patch

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,9 +7,17 @@ const Product = require('../models/productModel');
 
 // cause the incoming URL reqs will already be /products we only need to look for '/'
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handler for GET @ /products, yay!',
-  });
+  Product.find()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({ result });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post('/', (req, res, next) => {
@@ -25,42 +33,74 @@ router.post('/', (req, res, next) => {
     .save()
     .then((result) => {
       console.log(result);
+      if (result) {
+        res.status(201).json({
+          message: 'Handler for POST @ /products, yay!',
+          createdProduct: product,
+        });
+      } else {
+        res.status(404).json({
+          message: 'No item matches the id',
+        });
+      }
     })
     .catch((err) => {
       console.log(err);
+      res.status(500).json({ error: err });
     });
-  res.status(201).json({
-    message: 'Handler for POST @ /products, yay!',
-    createdProduct: product,
-  });
 });
 
 // in express :<any name afterwards> will handle reqs sent with the name specified
 // :<param> will appear in params so it can be extracted
-router.get('/:productID', (req, res, next) => {
-  const id = req.params.productID;
-  if (id === 'penguin') {
-    res.status(200).json({
-      messsage: 'Aha, a Linux user!',
-      id: id,
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  Product.findById(id)
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      // pass the error as well
+      res.status(500).json({ error: err });
     });
-  } else {
-    res.status(200).json({
-      messsage: 'Sigh',
-    });
+});
+
+router.patch('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  /**
+   * make an object of things to change in the db entry
+   * loop over the body of the request
+   * populate object with the changes from the request
+   */
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
   }
+  Product.update(
+    { _id: id },
+    // $set expects an object
+    { $set: updateOps }
+  )
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.patch('/:productID', (req, res, next) => {
-  res.status(200).json({
-    message: 'Products updated (but not really)',
-  });
-});
-
-router.delete('/:productID', (req, res, next) => {
-  res.status(200).json({
-    message: "Products deleted (please don't look for it)",
-  });
+router.delete('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 // export the router's handlers
